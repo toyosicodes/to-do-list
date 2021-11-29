0deeae078e1b245c243f5272dbe5c781aa2a91ad
Toyosi Salami <omotoyosisalami00@gmail.com>
2021-11-29 17:59:46 +0100

    back

---
+++ src/game/App.js
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import SingleCard from "./SingleCard";
+
+const cardImages = [
+  { src: "/photos/bey.jpg", matched: false },
+  { src: "/photos/ari.jpg", matched: false },
+  { src: "/photos/doja.jpg", matched: false },
+  { src: "/photos/lisa.jpg", matched: false },
+  { src: "/photos/tay.jpg", matched: false },
+  { src: "/photos/tems.jpg", matched: false },
+];
+
+function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setChoiceOne(null);
+    setChoiceTwo(null);
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  //handle a choice
+
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceTwo.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+  console.log(cards);
+
+  const resetTurn = (prevTurn) => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
+  };
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+      <p>Turns: {turns}</p>
+    </div>
+  );
+}
+
+export default App;
+//the key has to be inside the container of the result of the map function
+//useEffect runs automatically when the component first mounts to the dom
